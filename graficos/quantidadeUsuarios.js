13eebb86d0f9d7996dede2904831dcd034f6a858
Felipe Nader <felipenader22@gmail.com>
2025-09-01 10:29:19 -0300

    graficos

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,24 +1,23 @@
 async function quantidadeUsuarios() {
-    const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDosPostos = Object.keys(dados)
-    const quantidadeTrabalhadors = Object.values(dados)
+        const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
+                const res = await fetch(url)
+                const dados = await res.json()
+                const nomeDosPostos = Object.keys(dados)
+            const quantidadeTrabalhadores = Object.values(dados)
+            
 
+                const data = [
+                {
+                x: nomeDosPostos,
+                y:quantidadeTrabalhadores,
+                type: 'bar'
+    }
+ ]
 
-    const data = [
-        {
-            x: nomeDosPosto,
-            y: quantidadeTrabalhadors,
-            type: 'bar'
-        }
-    ]
-
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('grafico-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
-
+ const grafico = document.createElement('div')
+ grafico.className = 'grafico'
+ document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
 }
 
 quantidadeUsuarios()
